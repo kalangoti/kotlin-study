cf55ff05a6643430e2a5231880d6125d2c5b478e
kalangoti <kalangoti@gmail.com>
2022-09-02 20:53:31 -0300

    feat: KOTLIN - Conditions and loops

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,22 +1,32 @@
 fun main() {
     println("Hello world into Bytebank")
-    
+
     val titular = "Anderson"
     val numeroConta = 1000
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
     saldo -= 1000
-    
+
     println("Titular $titular")
     println("Número da Conta $numeroConta")
     println("Saldo da conta $saldo")
-    
+
+    // KOTLIN - Conditions and loops: https://kotlinlang.org/docs/control-flow.html
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
     when {
         saldo > 0.0 -> println("A conta é positiva")
         saldo == 0.0 -> {
             println("A conta é neutra")
         }
+
         else -> println("A conta é negativa")
     }
 }
