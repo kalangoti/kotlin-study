39c7398bba90eb30423d5593d4eef252d4149bfe
kalangoti <kalangoti@gmail.com>
2022-09-02 20:31:42 -0300

    feat: add when structure

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    print("Hello world to Bytebank")
+    println("Hello world into Bytebank")
+    
+    val titular = "Anderson"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+    saldo -= 1000
+    
+    println("Titular $titular")
+    println("Número da Conta $numeroConta")
+    println("Saldo da conta $saldo")
+    
+    when {
+        saldo > 0.0 -> println("A conta é positiva")
+        saldo == 0.0 -> {
+            println("A conta é neutra")
+        }
+        else -> println("A conta é negativa")
+    }
 }
